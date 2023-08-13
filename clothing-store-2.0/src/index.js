5534cd079749e103ef46e7e0e89d8b43106f46aa
Vukipoo <perovicv515@gmail.com>
2023-08-13 15:47:02 -0700

    changed BrowserRouter to HashRouter

--- clothing-store-2.0/src/index.js
+++ clothing-store-2.0/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 
 import App from "./App";
@@ -14,7 +14,7 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
       <CategoriesProvider>
         <CartProvider>
@@ -22,7 +22,7 @@ root.render(
         </CartProvider>
       </CategoriesProvider>
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
   
 );
